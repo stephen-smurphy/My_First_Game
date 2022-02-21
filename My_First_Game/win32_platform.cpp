014bf7a3a899c7c1eda18dcaba30cbed95275a14
Stephen Murphy <stephe1000@hotmail.com>
2022-02-21 21:16:26 +0000

    Episode 2: Drawing Graphics

--- My_First_Game/win32_platform.cpp
+++ My_First_Game/win32_platform.cpp
@@ -1,6 +1,17 @@
+#include "utils.cpp"
 #include <windows.h>
 
-bool running = true;
+global_variable bool running = true;
+
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+#include "renderer.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -8,6 +19,26 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 		case WM_CLOSE:
 		case WM_DESTROY: {
 			running = false;
+		} break;
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int buffer_size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
 		} break;
 
 		default: {
@@ -34,6 +65,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	//Create Window
 	HWND window = CreateWindow(window_class.lpszClassName, L"My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
 
 	//Enter the game loop!
 	while (running) {
@@ -45,7 +77,10 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		}
 
 		//Simulate
+		clear_screen(0xff0000);
+		draw_rect(0, 0, .5, .2, 0x00ff22);
 
 		//Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
