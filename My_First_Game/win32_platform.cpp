e3caf1828f3fc00e8c2a57a47881e08612943111
Stephen Murphy <stephe1000@hotmail.com>
2022-02-21 20:08:17 +0000

    Episode 1: Window and Pointers

---
+++ My_First_Game/win32_platform.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT result = 0;
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	//To Draw a window you need:
+	//1. A window class
+	//2. Register window class
+	//3. Create window
+
+	//Create window class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = L"Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+
+	//Register Class
+	RegisterClass(&window_class);
+
+	//Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, L"My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+	//Enter the game loop!
+	while (running) {
+		//Input
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		//Simulate
+
+		//Render
+	}
+}
